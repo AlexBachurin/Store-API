cfdc4c21919a4f31f58f057fea8877a0f7620b97
Aleksandr <alexbachurin91@gmail.com>
2023-03-19 11:34:22 +0300

    query params/query object handling

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,21 @@
+const ProductModel = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "products testing route" });
+  const products = await ProductModel.find({});
+  res.status(200).json({ products });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  // this way we wont get empty array in return if query params doesnt exist
+  const { featured } = req.query;
+  // if user isnt looking for featured, then we will use Model.Find({}) and get all items back
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  const products = await ProductModel.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
