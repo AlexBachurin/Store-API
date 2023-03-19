ed7c1713e85ad5ddbb057604324ae12fb24b21af
Aleksandr <alexbachurin91@gmail.com>
2023-03-19 11:50:46 +0300

    company and name filtering using regex

--- controllers/products.js
+++ controllers/products.js
@@ -7,13 +7,19 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
   // this way we wont get empty array in return if query params doesnt exist
-  const { featured } = req.query;
-  // if user isnt looking for featured, then we will use Model.Find({}) and get all items back
+  const { featured, company, name } = req.query;
+  // if user isnt looking for queries that in the schema,
+  // then we will use Model.Find({}) with empty object and get all items back
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
-
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
   const products = await ProductModel.find(queryObject);
   res.status(200).json({ products, nbHits: products.length });
 };
