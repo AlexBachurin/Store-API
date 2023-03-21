76f55d0daa369ca9328246414ceef611bcdd7b77
Aleksandr <alexbachurin91@gmail.com>
2023-03-21 23:33:52 +0300

    numeric filters functinality completed(sort by price, rating or any numeric value to add in future)

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
   // this way we wont get empty array in return if query params doesnt exist
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   // if user isnt looking for queries that in the schema,
   // then we will use Model.Find({}) with empty object and get all items back
   const queryObject = {};
@@ -20,6 +20,32 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
+  if (numericFilters) {
+    // transform to user friendly expressions to mongoose expressions with regexp
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eg",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(regEx, (match) => {
+      return `-${operatorMap[match]}-`;
+    });
+    // options on which we can use numeric filters
+    const options = ["price", "rating"];
+    // since in api can be 2 filters for numeric values, we can split it
+    filters = filters.split(",").forEach((item) => {
+      // split the item, since it will be in this format(price-$gt-40)
+      const [field, operator, value] = item.split("-");
+      // check if field in options , if it is then all ok
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+
   let result = ProductModel.find(queryObject);
   // sort
   if (sort) {
